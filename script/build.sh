7346c1f812616ba9797d5aed7d39d46c2f332856
Mario Savarese <57295823+marsavar@users.noreply.github.com>
2024-06-26 15:02:22 +0100

    Package binaries into tarballs (#12)
    * Package binary into a tarball

--- script/build.sh
+++ script/build.sh
@@ -2,12 +2,44 @@
 
 set -e
 
-mkdir -p bin
+pushd () {
+    command pushd "$@" > /dev/null
+}
 
-# Targeting AMD (x86) Mac 
-env GOOS=darwin GOARCH=amd64 go build -o bucketblocker-darwin-amd64 main.go
-mv bucketblocker-darwin-amd64 bin/
+popd () {
+    command popd > /dev/null
+}
 
-# Targeting ARM64 Mac (Apple silicon) 
-env GOOS=darwin GOARCH=arm64 go build -o bucketblocker-darwin-arm64 main.go
-mv bucketblocker-darwin-arm64 bin/
+SCRIPT_PATH=$( cd "$(dirname "$0")" ; pwd -P )
+
+pushd "$SCRIPT_PATH/.."
+
+APP=bucketblocker
+
+# We only build for Mac OS
+ARCHITECTURES=("arm64" "amd64")
+
+for ARCH in "${ARCHITECTURES[@]}"; do
+    echo ""
+    echo "=== Creating release for Darwin $ARCH ==="
+    GOOS=darwin GOARCH=$ARCH go build -o "$APP" main.go
+
+    mkdir -p "release/darwin-$ARCH"
+
+    TAR_NAME="${APP}_darwin-${ARCH}.tar.gz"
+    tar -czf "release/darwin-${ARCH}/${TAR_NAME}" "$APP"
+
+    pushd "release/darwin-$ARCH"
+
+    SHA256_SUM=$(shasum -a 256 "$TAR_NAME" | awk '{ print $1 }')
+    echo "The following is the SHA256 sum for the '$TAR_NAME' bundle:"
+    echo "$SHA256_SUM"
+
+    popd
+
+    ## Create build summary if running in Github Actions
+    if [ -f "$GITHUB_STEP_SUMMARY" ]; then
+        echo "### Darwin ${ARCH} SHA256 sum" >> $GITHUB_STEP_SUMMARY 
+        echo "$SHA256_SUM" >> $GITHUB_STEP_SUMMARY 
+    fi
+done
