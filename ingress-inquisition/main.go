593d2ea2a76b193f146af3bbc17587090b303706
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-08-15 13:55:20 +0100

    refactor: move verbose logging logic into its own function collect security groups that have netrowk interfaces attached to them

--- ingress-inquisition/main.go
+++ ingress-inquisition/main.go
@@ -11,36 +11,9 @@ import (
 	"github.com/guardian/fsbp-tools/ingress-inquisition/utils"
 )
 
-func main() {
-
-	ctx := context.Background()
-
-	args := utils.ParseArgs()
-
-	cfg, err := awsauth.LoadDefaultConfig(ctx, args.Profile, args.Region)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
-
-	securityHubClient := securityhub.NewFromConfig(cfg)
-
-	ec2Client := ec2.NewFromConfig(cfg)
-
-	findings, err := awsauth.ReturnFindings(ctx, securityHubClient, "EC2.2", 100)
-	if err != nil {
-		log.Fatalf("Error getting findings: %v", err)
-	}
-
-	securityGroups := []string{}
-	for _, finding := range findings.Findings {
-		for _, resource := range finding.Resources {
-			sgId := utils.IdFromArn(*resource.Id)
-			securityGroups = append(securityGroups, sgId)
-		}
-	}
-
+func logGroupsAndVpcs(ctx context.Context, ec2Client *ec2.Client, groupIds []string) {
 	groupDescriptions, err := ec2Client.DescribeSecurityGroups(ctx, &ec2.DescribeSecurityGroupsInput{
-		GroupIds: securityGroups,
+		GroupIds: groupIds,
 	})
 	if err != nil {
 		log.Fatalf("Error describing security groups: %v", err)
@@ -56,6 +29,53 @@ func main() {
 		for _, vpc := range vpcs.Vpcs {
 			name := utils.FindTag(vpc.Tags, "Name", "unknown")
 			fmt.Printf("Security group: %s, VPC name: %s, VPC id: %s\n", *group.GroupId, name, *group.VpcId)
+		}
+	}
+}
+
+func main() {
+
+	ctx := context.Background()
+
+	args := utils.ParseArgs()
+
+	cfg, err := awsauth.LoadDefaultConfig(ctx, args.Profile, args.Region)
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+
+	securityHubClient := securityhub.NewFromConfig(cfg)
+
+	findings, err := awsauth.ReturnFindings(ctx, securityHubClient, "EC2.2", 100)
+	if err != nil {
+		log.Fatalf("Error getting findings: %v", err)
+	}
+
+	securityGroups := []string{}
+	for _, finding := range findings.Findings {
+		for _, resource := range finding.Resources {
+			sgId := utils.IdFromArn(*resource.Id)
+			securityGroups = append(securityGroups, sgId)
+		}
+	}
+
+	ec2Client := ec2.NewFromConfig(cfg)
+
+	logGroupsAndVpcs(ctx, ec2Client, securityGroups)
+
+	maxInterfaceResults := int32(1000)
+
+	securityGroupsInNetworkInterfaces := []string{}
+
+	res, err := ec2Client.DescribeNetworkInterfaces(ctx, &ec2.DescribeNetworkInterfacesInput{
+		MaxResults: &maxInterfaceResults,
+	})
+	if err != nil {
+		log.Fatalf("Error describing network interfaces: %v", err)
+	}
+	for _, networkInterface := range res.NetworkInterfaces {
+		for _, group := range networkInterface.Groups {
+			securityGroupsInNetworkInterfaces = append(securityGroupsInNetworkInterfaces, *group.GroupId)
 		}
 	}
 }
