5fde363340509dfd3a1146139a43a0c03f1f0da9
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-09-11 16:38:32 +0100

    ci: use variables for good and bad names

--- bucket-blocker/script/dash-lint.sh
+++ bucket-blocker/script/dash-lint.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
+TOOL_NAME="bucket-blocker"
+INCORRECT_NAME="bucketblocker"
+
 CURRENT_FILE=$(basename "${BASH_SOURCE}")
 ROOT_DIR=$(git rev-parse --show-toplevel)
-BAD_NAMES=$(grep -ril "bucketblocker" "$ROOT_DIR" --exclude "$CURRENT_FILE")
+BAD_NAMES=$(grep -ril "$INCORRECT_NAME" "$ROOT_DIR" --exclude "$CURRENT_FILE")
 
 if [ -n "$BAD_NAMES" ]; then
-    echo "bucket-blocker has a dash in it, and inconsistencies in the codebase can cause major problems, please correct the spelling." >&2
-    printf "The following files contain the incorrect string 'bucketblocker':\n\n" >&2
+    echo "$TOOL_NAME has a dash in it, and inconsistencies in the codebase can cause major problems, please correct the spelling." >&2
+    printf "The following files contain the incorrect string '%s':\n\n" "$INCORRECT_NAME" >&2
     echo "$BAD_NAMES" >&2
     exit 1;
 else
-    echo "No files contain the name 'bucketblocker'"
+    echo "No files contain the name '$INCORRECT_NAME'."
     exit 0;
 fi
