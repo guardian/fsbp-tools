cda4489a93f1f8c4e8046cc9b51bc552abd1c5a9
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-09-12 18:03:05 +0100

    Run dash-lint on ingress-inquisition (#36)
    * ci: use variables for good and bad names
    
    * ci: move dash-lint to root
    
    * ci: parameterise correct and incorrect spellings so we can check ingress-inquisition
    
    * ci: create dashless string from input
    
    * ci: make dash checking one job

---
+++ script/dash-lint.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+TOOL_NAME=$1
+INCORRECT_NAME=$(echo "$TOOL_NAME" | tr -d -)
+
+CURRENT_FILE=$(basename "${BASH_SOURCE}")
+ROOT_DIR=$(git rev-parse --show-toplevel)
+BAD_NAMES=$(grep -ril "$INCORRECT_NAME" "$ROOT_DIR" --exclude "$CURRENT_FILE")
+
+if [ -n "$BAD_NAMES" ]; then
+    echo "$TOOL_NAME has a dash in it, and inconsistencies in the codebase can cause major problems, please correct the spelling." >&2
+    printf "The following files contain the incorrect string '%s':\n\n" "$INCORRECT_NAME" >&2
+    echo "$BAD_NAMES" >&2
+    exit 1;
+else
+    echo "No files contain the name '$INCORRECT_NAME'."
+    exit 0;
+fi
