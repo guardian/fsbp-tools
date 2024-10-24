501ac5b9d3b7f55ef2676b722d0b9cdcb4973616
Natasha <67543397+NovemberTang@users.noreply.github.com>
2024-10-24 08:23:50 +0100

    ci: check a couple more common delimiters

--- script/dash-lint.sh
+++ script/dash-lint.sh
@@ -1,18 +1,26 @@
 #!/usr/bin/env bash
 
 TOOL_NAME=$1
-INCORRECT_NAME=$(echo "$TOOL_NAME" | tr -d -)
-
 CURRENT_FILE=$(basename "${BASH_SOURCE}")
 ROOT_DIR=$(git rev-parse --show-toplevel)
-BAD_NAMES=$(grep -ril "$INCORRECT_NAME" "$ROOT_DIR" --exclude "$CURRENT_FILE" --exclude-dir=".git")
 
-if [ -n "$BAD_NAMES" ]; then
-    echo "$TOOL_NAME has a dash in it, and inconsistencies in the codebase can cause major problems, please correct the spelling." >&2
-    printf "The following files contain the incorrect string '%s':\n\n" "$INCORRECT_NAME" >&2
-    echo "$BAD_NAMES" >&2
-    exit 1;
-else
-    echo "No files contain the name '$INCORRECT_NAME'."
-    exit 0;
-fi
+function bad_delimiter_check() {
+    DELIMITER=$1
+    # shellcheck disable=SC2116
+    WRONG_NAME=$(echo "${TOOL_NAME//-/$DELIMITER}")
+    echo "Checking for $WRONG_NAME in the codebase..."
+    FAILURES=$(grep -ril "$WRONG_NAME" "$ROOT_DIR" --exclude "$CURRENT_FILE" --exclude-dir=".git")
+
+    if [ -n "$FAILURES" ]; then
+        echo "$TOOL_NAME has a dash in it, and inconsistencies in the codebase can cause major problems, please correct the spelling." >&2
+        printf "The following files contain the incorrect string '%s':\n\n" "$WRONG_NAME" >&2
+        echo "$FAILURES" >&2
+        exit 1;
+    else
+        printf "No files contain the erroneous name: '%s'.\n\n" "$WRONG_NAME"
+    fi
+}
+
+bad_delimiter_check "_"
+bad_delimiter_check " "
+bad_delimiter_check ""
