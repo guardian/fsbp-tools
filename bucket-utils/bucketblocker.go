a31c76e4c700b47e720e82a41df57a2ee7e0a022
Natasha <67543397+NovemberTang@users.noreply.github.com>
2025-06-12 13:06:06 +0100

    refactor: pass in cfg.Region instead of region

--- bucket-utils/bucketblocker.go
+++ bucket-utils/bucketblocker.go
@@ -4,17 +4,14 @@ import (
 	"context"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/cloudformation"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go-v2/service/securityhub"
 	"github.com/guardian/fsbp-tools/fsbp-fix/common"
 )
 
-func FixS3_8(ctx context.Context, profile string, region string, bucketCount int, exclusions []string, execute bool) {
-	cfg, err := common.Auth(ctx, profile, region)
-	if err != nil {
-		log.Fatalf("unable to load SDK config, %v", err)
-	}
+func FixS3_8(ctx context.Context, cfg aws.Config, bucketCount int, exclusions []string, execute bool) {
 
 	accountId, err := common.GetAccountId(ctx, cfg)
 	if err != nil {
@@ -24,7 +21,7 @@ func FixS3_8(ctx context.Context, profile string, region string, bucketCount int
 	securityHubClient := securityhub.NewFromConfig(cfg)
 	s3Client := s3.NewFromConfig(cfg)
 	cfnClient := cloudformation.NewFromConfig(cfg)
-	bucketsToBlock, err := FindBucketsToBlock(ctx, securityHubClient, s3Client, cfnClient, int32(bucketCount), exclusions, accountId, region)
+	bucketsToBlock, err := FindBucketsToBlock(ctx, securityHubClient, s3Client, cfnClient, int32(bucketCount), exclusions, accountId, cfg.Region)
 	if err != nil {
 		log.Fatalf("Error working out which buckets need blocking: %v", err)
 	}
--- vpc-utils/ingressinquisition.go
+++ vpc-utils/ingressinquisition.go
@@ -5,17 +5,13 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/ec2"
 	"github.com/aws/aws-sdk-go-v2/service/securityhub"
 	"github.com/guardian/fsbp-tools/fsbp-fix/common"
 )
 
-func FixEc2_2(ctx context.Context, profile string, region string, execute bool) { //TODO does this need to be a pointer?
-
-	cfg, err := common.Auth(ctx, profile, region)
-	if err != nil {
-		log.Fatalf("%v", err)
-	}
+func FixEc2_2(ctx context.Context, cfg aws.Config, execute bool) {
 
 	ec2Client := ec2.NewFromConfig(cfg)
 	securityHubClient := securityhub.NewFromConfig(cfg)
@@ -25,7 +21,7 @@ func FixEc2_2(ctx context.Context, profile string, region string, execute bool) 
 		log.Fatalf("Error getting account ID: %v", err)
 	}
 
-	securityGroupRuleDetails, err := FindUnusedSecurityGroupRules(ctx, ec2Client, securityHubClient, accountId, region)
+	securityGroupRuleDetails, err := FindUnusedSecurityGroupRules(ctx, ec2Client, securityHubClient, accountId, cfg.Region)
 
 	if err != nil {
 		log.Fatalf("Error finding unused security group rules: %v", err)
